2a842b7b9bef14130da36cc264f549a38f4ce15e
rakesh <rakeshavulapati1999@gmail.com>
2023-09-26 13:01:32 +0530

    for_each

--- foreach/ec2.tf
+++ foreach/ec2.tf
@@ -1,3 +1,20 @@
 resource  "aws_instance" "roboshop"{
-    ami = var.amid_id
+    for_each = var.instances
+    ami = var.ami_id
+    security_groups = [ aws_security_group.allow_all.name ]
+    instance_type = each.value
+    tags = {
+        Name = each.key
+    }
 }
+resource "aws_route53_record" "DNS" {
+    for_each = aws_instance.roboshop
+    zone_id = var.zone_id
+    name = "${each.key}.${var.domain}"
+    type = "A"
+    ttl = 1
+    records = [ each.key == "web" ? each.value.public_ip : each.value.private_ip ]
+}
+output "aws_instance_info" {
+  value = aws_instance.roboshop[*].private_ip
+}  
--- foreach/variables.tf
+++ foreach/variables.tf
@@ -1,1 +1,67 @@
-variable 
+variable "ami_id" {
+    default = "ami-03265a0778a880afb"
+}
+
+variable "instances" {
+
+    default = {
+
+        mongodb = "t3.medium"
+        MySQL = "t3.medium"
+        Redis = "t2.micro"
+        RabbitMQ = "t2.micro"
+        Catalogue = "t2.micro"
+        User = "t2.micro"
+        Cart = "t2.micro"
+        Shipping = "t2.micro"
+        Payment = "t2.micro"
+        Web = "t2.micro"
+
+    }
+  
+}
+ variable "sg" {
+
+    default = "all"
+   
+ }
+
+ variable "sg_cidr" {
+
+    default = ["0.0.0.0/0"]
+   
+ }
+
+ variable "zone_id" {
+    default = "Z08892663AT899M4JPPZH"
+ }
+ variable "domain" {
+    default = "myroboshop.site" 
+ }
+ variable "ingress" {
+    
+    type = list
+    default = [
+    {
+        description      = "Allowing 80 inbound traffic"
+        from_port        = 80 # this is number
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+        description      = "Allowing 443 inbound traffic"
+        from_port        = 443 # this is number
+        to_port          = 443
+        protocol         = "https"
+        cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+        description      = "Allowing 22 inbound traffic"
+        from_port        = 22 # this is number
+        to_port          = 22
+        protocol         = "ssh"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ]
+ }
